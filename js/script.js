c8ee807a77f73c4f6f3345465534f37b34d74d13
y860 <yvonnebosire24@gmail.com>
2022-11-20 16:24:22 +0300

    new changes to log in and sign up

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ document.querySelector('#login-btn').onclick =() =>{
 }
 let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick =() =>{
+document.querySelector('#menu-bars').onclick =() =>{
     navbar.classList.toggle('active');
     shoppingCart.classList.remove('active');
     searchForm.classList.remove('active');
@@ -37,3 +37,24 @@ window.onscroll = () =>{
     searchForm.classList.remove('active');
     LoginForm.classList.remove('active');
 }
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction:false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
